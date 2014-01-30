01e6486983a05b2f8b90783c1e318dc5ed422a61
warmasterfarlop <warmasterfarlop@gmail.com>
2014-01-30 21:42:45 +0100

    Nuevo proyecto de prueba. En este proyecto se testea una clase que hace de interfaz con la App de contactos del movil.

---
+++ GuardarInfoContactos/src/com/example/guardarinfocontactos/AgregarContacto.java
@@ -0,0 +1,14 @@
+package com.example.guardarinfocontactos;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AgregarContacto extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.agregar_contacto);
+	}
+	
+}
